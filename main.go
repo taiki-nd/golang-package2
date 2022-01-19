39aa579b3a776c389f08f94e569e4be0bf7f6dfa
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-19 21:59:28 +0900

    sortに関する記述

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"time"
 )
 
@@ -51,4 +52,25 @@ func main() {
 	fmt.Println(fs)
 	fss := r2.FindStringSubmatch("/view/test")
 	fmt.Println(fss, fss[0], fss[1], fss[2])
+
+	//sort
+	fmt.Println("---sort---")
+	i := []int{5, 3, 2, 8, 7}
+	s := []string{"d", "a", "f"}
+	p := []struct {
+		Name string
+		Age  int
+	}{
+		{"Nancy", 20},
+		{"Vera", 40},
+		{"Mike", 30},
+		{"Bob", 50},
+	}
+	fmt.Println(i, s, p)
+	sort.Ints(i)
+	sort.Strings(s)
+	sort.Slice(p, func(i, j int) bool { return p[i].Name < p[j].Name }) //less関数
+	fmt.Println(i, s, p)
+	sort.Slice(p, func(i, j int) bool { return p[i].Age < p[j].Age })
+	fmt.Println(i, s, p)
 }
